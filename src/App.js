5161afee44034eae3c2a3abe5a81d29bf958d99a
Larisa <larisashapkinab@gmail.com>
2021-11-19 13:22:12 -0500

    Basic CSS, show cart items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,131 @@
+import { Component } from "react";
 import "./App.css";
+import data from './data/productData';
 
-const App = () => {
-  return <h1>Hello, world!</h1>;
-};
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      shoppingList: data,
+      shoppingArr:[],
+      subtotal: 0,
+
+      firstName: "",
+      lastName: "",
+      email: "",
+      creditCard: "",
+      zipCode: ""
+    }
+  }
+
+  handleitemClick=(item)=>{
+    this.setState({
+      shoppingArr: [...this.state.shoppingArr, item],
+      
+    })
+  }
+
+  // handleSubtotal=(item)=>{
+  //   let subtotal = 0;
+    
+  //   this.setState({
+  //     subtotal: subtotal+ item.price
+  //   })
+  // }
+  
+  handleFirstName =(e)=>{
+    this.setState({
+
+    })
+  }
+  render(){
+    let shoppingList= this.state.shoppingList.map((item)=>{
+      return (
+        <div className="shoppingcart-item-container"
+            onClick= {()=>this.handleitemClick(item)}
+        >
+            <div><strong>{item.name}</strong></div>
+            <div>Price: ${item.price}</div>
+            <button type="Submit">Add to cart</button>
+            <div>{item.description}</div>
+            <img src={item.img} alt="itme image"/>
+        </div>
+        
+      )
+    })
+
+      
+    let shoppingItemArr = this.state.shoppingArr.map((item)=>{
+      return(
+        <div>
+          <li>{item.name}: ${item.price}</li>
+          
+        </div>
+      )
+    })
+    return(
+      <div className="app" id="app-container">
+        <h1>My Garage Sale</h1>
+        <div class="shoppingcart-list-container">
+          
+            {shoppingList}
+          
+          <div class="form">
+            <h2>Cart</h2>
+            <ul>
+                {shoppingItemArr}
+            </ul>
+                
+            <div><strong>Subtotal: {this.handleSubtotal}</strong></div>
+            <div><strong>Tax:</strong></div>
+            <div><strong>Total:</strong></div>
+      
+            <h2>Checkout</h2>
+          
+          <form>
+            <label htmlFor="first-name">First Name</label>
+            <input
+              // value={}
+              type="text"
+              name="first-name"
+              id="first-name"
+            />
+            <br></br>
+            <label htmlFor="last-name">Last Name</label>
+            <input
+              type="text"
+              name="last-name"
+              id="last-name"
+            />
+            <br></br>
+            <label htmlFor="first-name">Email</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+            />
+            <br></br>
+            <label htmlFor="credit-card">Credit Card</label>
+            <input
+              type="text"
+              name="credit-card"
+              id="credit-card"
+            />
+            <br></br>
+            <label htmlFor="zipcode">Zip Code</label>
+            <input
+              type="text"
+              name="zipcode"
+              id="zipcode"
+            />
+            <br></br>
+            <button type="submit">Buy Now</button>
+          </form>
+        </div>
+      </div>   
+     </div>  
+    )
+  }
+}
 
 export default App;
